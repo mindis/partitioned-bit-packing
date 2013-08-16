5c0b6592ebafffa9cd1b21c1fd2b57deb06c7370
Pedro <pedroflemming@gmail.com>
2013-08-16 13:52:57 +0200

    virtual destructor added to AbstractColumn

--- sources/lib/database/AbstractColumn.h
+++ sources/lib/database/AbstractColumn.h
@@ -6,6 +6,7 @@
 template <typename _T_VALUE>
 class AbstractColumn {
 public:
+	virtual ~AbstractColumn() {}
     virtual void push_back(const _T_VALUE&) = 0;
     virtual _T_VALUE& get(const uint) = 0;
     virtual size_t size() = 0;
